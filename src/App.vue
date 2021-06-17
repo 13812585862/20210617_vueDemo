ec2080587a05f7eec16e6cdb3866d046a715a529
yuwenbo <1220693763@qq.com>
2021-06-17 22:26:01 +0800

    init vue_demo

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+     <router-view></router-view>
+    <FooterGuide v-show="$route.meta.showFooter"></FooterGuide>
+  </div>
+</template>
+
+<script>
+import FooterGuide  from "./components/FooterGuide/FooterGuide";
+export default {
+  components:{
+    FooterGuide
+  }
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+.app
+  width 100%
+  height 100%
+  background #f5f5f5
+</style>
+<style>
+/*#app {*/
+/*  font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  text-align: center;*/
+/*  color: #2c3e50;*/
+/*  margin-top: 60px;*/
+/*}*/
+</style>
